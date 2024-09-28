1dd6277fa0b3a9ad494cdc87987f6af7a624ed47
Erik Veraksich <erik.veraksich@yandex.ru>
2024-09-28 23:28:00 +0300

    Добавил пользовательскую кнопку ButtonPanel (пустая)

---
+++ ButtonPanel.h
@@ -0,0 +1,56 @@
+#pragma once
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+
+namespace UnsaintedControls {
+
+	/// <summary>
+	/// —водка дл€ ButtonPanel
+	/// </summary>
+	public ref class ButtonPanel : public System::Windows::Forms::UserControl
+	{
+	public:
+		ButtonPanel(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~ButtonPanel()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
